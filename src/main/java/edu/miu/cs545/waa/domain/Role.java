d7fbeb2d3cb238e69d61dae24b13007d02e9859c
sujanmaka <suzanmaka@gmail.com>
2022-04-17 17:34:04 -0500

    fixes

--- src/main/java/edu/miu/cs545/waa/domain/Role.java
+++ src/main/java/edu/miu/cs545/waa/domain/Role.java
@@ -1,9 +1,6 @@
 package edu.miu.cs545.waa.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 import edu.miu.cs545.waa.enums.RoleType;
 import lombok.Data;
@@ -19,5 +16,6 @@ public class Role {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
+  @Enumerated(EnumType.STRING)
   private RoleType roleType;
 }
