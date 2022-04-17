57084ec17216896a76d2bc3c59cf4c0fbcbad016
sujanmaka <suzanmaka@gmail.com>
2022-04-16 21:56:04 -0500

    first commit

---
+++ src/main/java/edu/miu/cs545/waa/WaaApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs545.waa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaaApplication.class, args);
+    }
+
+}
